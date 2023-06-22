25d65ad580f23f72609e6f70ced833751bc5084e
Jamie McCrae <jamie.mccrae@nordicsemi.no>
2023-06-22 12:43:44 +0100

    test

---
+++ mcumgr/grp/example/include/example_mgmt.h
@@ -0,0 +1,43 @@
+    /*
+     * Copyright (c) 2023 Nordic Semiconductor ASA
+     *
+     * SPDX-License-Identifier: Apache-2.0
+     */
+
+    #ifndef H_EXAMPLE_MGMT_
+    #define H_EXAMPLE_MGMT_
+
+    #ifdef __cplusplus
+    extern "C" {
+    #endif
+
+    /**
+     * Group ID for example management group.
+     */
+    #define MGMT_GROUP_ID_EXAMPLE  MGMT_GROUP_ID_PERUSER
+
+    /**
+     * Command IDs for example management group.
+     */
+    #define EXAMPLE_MGMT_ID_TEST   0
+    #define EXAMPLE_MGMT_ID_OTHER  1
+
+    /**
+     * Command result codes for example management group.
+     */
+    enum example_mgmt_ret_code_t {
+            /** No error, this is implied if there is no ret value in the response */
+            EXAMPLE_MGMT_RET_RC_OK = 0,
+
+            /** Unknown error occurred. */
+            EXAMPLE_MGMT_RET_RC_UNKNOWN,
+
+            /** The provided value is not wanted at this time. */
+            EXAMPLE_MGMT_RET_RC_NOT_WANTED,
+    };
+
+    #ifdef __cplusplus
+    }
+    #endif
+
+    #endif
